cdb7e06020cb20b6bcac06b8c9e6de279ed68afa
Vladimir Iakovlev <nvbn.rm@gmail.com>
2017-05-26 18:21:44 +0200

    #N/A: Bump version, add missing dependencies

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-VERSION = '0.1.1'
+VERSION = '0.2'
 
 install_requires = ['py-backwards']
+extras_require = {':python_version<"3.4"': ['pathlib2'],
+                  ':python_version<"3.5"': ['typing']}
 
 setup(name='py-backwards-packager',
       version=VERSION,
@@ -16,5 +18,6 @@ setup(name='py-backwards-packager',
       include_package_data=True,
       zip_safe=False,
       install_requires=install_requires,
+      extras_require=extras_require,
       entry_points={'console_scripts': [
           'py-backwards-packager = py_backwards_packager.main:main']})
